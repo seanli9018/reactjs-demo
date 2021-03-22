46dc80fde6be095189176bad592ef7d70df8e8cc
seanli <seanli9018@gmail.com>
2021-03-21 19:36:53 -0500

    Updates

---
+++ static/js/0.14db0482.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptodo-list"]=this["webpackJsonptodo-list"]||[]).push([[0],{274:function(e){e.exports=JSON.parse('{"usercenterTitle":"This is user center page.","usercenterContainer":[{"title":"User info","description":"This is user description"}],"tasksInputPlaceholder":"Input the task you need to accomplish, then press enter key!"}')}}]);
+//# sourceMappingURL=0.14db0482.chunk.js.map
